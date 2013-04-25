7a715ee777e5420a69827ce985f9fc8a7d19fd4e
Ben Draut <drautb@gmail.com>
2013-04-25 15:51:38 -0600

    Enabled Close Button

--- setup/src/SetupApp.cpp
+++ setup/src/SetupApp.cpp
@@ -11,7 +11,7 @@ bool SetupApp::OnInit()
     SetupFrame *frame = new SetupFrame(_("Greebles Setup"), 
 									   wxPoint(50, 50),
 									   wxSize(APP_WIDTH, APP_HEIGHT),
-									   wxSYSTEM_MENU |  wxCAPTION | wxFRAME_TOOL_WINDOW);
+									   wxSYSTEM_MENU |  wxCAPTION | wxCLOSE_BOX | wxSTAY_ON_TOP | wxFRAME_TOOL_WINDOW);
 
     frame->Show(true);
 
