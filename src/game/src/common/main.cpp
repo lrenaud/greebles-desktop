ef6836f4fbb8eb959d935048b07732df8b82d773
Ben Draut <drautb@gmail.com>
2013-08-26 20:25:24 -0600

    Added ContentIds file, started listing them.

--- src/game/src/common/main.cpp
+++ src/game/src/common/main.cpp
@@ -1,7 +1,19 @@
+#include <IL/il.h>
+#include <IL/ilut.h>
+
+#include <util/Log.h>
+
 #include "GreeblesGame.h"
 
 int main(int argc, char* argv[])
 {
+    // Setup DevIL to use OpenGL
+    ilInit();
+    iluInit();
+    ilutRenderer(ILUT_OPENGL);
+
+    SOAR::Util::Log::GetInstance()->SetFilterLevel(SOAR::Util::Log::LOG_ALL);
+
     GreeblesGame::GetInstance().Run();
     
     return 0;
